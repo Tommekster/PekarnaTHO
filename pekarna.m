656e500ba438e24c3e7bb731a39955b4747e43fb
Tomáš Zikmund <tommekster@gmail.com>
2018-01-03 12:42:42 +0100

    oprava casu, pridany vystupni cisla

--- pekarna.m
+++ pekarna.m
@@ -1,8 +1,11 @@
-function pekarna(pocetPrepazek,maxFronta,dobaCekani,zakazniciph,...
+function [zakaznici,prumerna_delka_fronty,...
+    prumerna_doba_v_pekarne,prumerna_doba_ve_fronte,...
+    pocet_obslouzenych, pocet_neobslouzenych, trzba, usla_trzba] = ...
+    pekarna(pocetPrepazek,maxFronta,dobaCekani,zakazniciph,...
     dobaObsluhy,utrata,pocatecniFronta,dobaSimulace)
 if nargin < 1, pocetPrepazek = 1; end % pocet prepazek
 if nargin < 2, maxFronta = Inf; end
-if nargin < 3, dobaCekani = 15; end % ochota zakaznika pockat si
+if nargin < 3, dobaCekani = 60; end % ochota zakaznika pockat si
 if nargin < 4, zakazniciph = 100; end % stredni pocet cestujicich k odbaveni za hodinu
 if nargin < 5, dobaObsluhy = 2; end % stredni doba stravena na prepazce v minutach
 if nargin < 6, utrata = 100; end
@@ -21,8 +24,8 @@ vObsluze = [ ];
 obslouzeno = 0;
 
 prichod = time+exprnd(1/zakazniciph);
-dalsiOdchod = dobaSimulace;
-dalsiObsluha = dobaSimulace;
+dalsiOdchod = Inf;
+dalsiObsluha = Inf;
 
 % pocatecni fronta
 for i = 1:pocatecniFronta
@@ -36,14 +39,14 @@ while time < dobaSimulace
     % Buï prijde zakaznik nebo zakaznik odejde (skonci obsluha u prepazky)
     
     % bud se nikdo neobsluhuje, nebo prisel driv nez skonci nektery obsluhovany.
-    if (prichod < min(dalsiObsluha, dalsiOdchod))
+    if (prichod < min([Inf,dalsiObsluha,dalsiOdchod]))
         % zapisu ze prisel zakaznik
         prichodZakaznika(prichod);
         % posunu hodiny
         time = prichod;
         % pripravi se cas prichodu dalsiho cloveka
         prichod = time + exprnd(1/zakazniciph); 
-    elseif dalsiOdchod < dalsiObsluha % zakaznikovi dosla trpelivost
+    elseif dalsiOdchod < min([Inf,dalsiObsluha]) % zakaznikovi dosla trpelivost
         [time,clovek] = min(veFronte(:,2));
         opustFrontu(clovek)
     else % nastane dalsiObsluha (nekdo odesel od prepazky)
@@ -60,18 +63,28 @@ while time < dobaSimulace
 end
 
 %neobslouzeni zakazniku po zaviracce
-%zakaznici(end-fronta+1:end,2:4)=ones(fronta,1)*[time time 0];
+for opozdilec = veFronte(:,1)
+    zakaznici(opozdilec,2) = time;
+    zakaznici(opozdilec,3) = time;
+    zakaznici(opozdilec,4) = 0;
+end
 
 % vypocet prumerne delky fronty
 prumerna_delka_fronty=sum(diff(fronta_hist(:,1)).*fronta_hist(1:end-1,2))/fronta_hist(end-1,1);
 %vypocet pozadovanych casu [v min]
 obslouzeni = zakaznici(zakaznici(:,4) == 1,:);
+odmitnuti = zakaznici(zakaznici(:,4) == 0,:);
 doba_v_pekarne=(obslouzeni(:,3)-obslouzeni(:,1))*60;
 doba_ve_fronte=(obslouzeni(:,2)-obslouzeni(:,1))*60;
 prumerna_doba_v_pekarne=mean(doba_v_pekarne);
 prumerna_doba_ve_fronte=mean(doba_ve_fronte);
+pocet_obslouzenych = sum(zakaznici(:,4) == 1);
+pocet_neobslouzenych = sum(zakaznici(:,4) == 0);
+trzba = sum(obslouzeni(:,5));
+usla_trzba = sum(odmitnuti(:,5));
 
 % nakresli vysledek 
+if nargout < 1
     figure
     subplot(2,2,[1,3])
     vykresliFrontu
@@ -79,6 +92,17 @@ prumerna_doba_ve_fronte=mean(doba_ve_fronte);
     vykresliDobu
     subplot(2,2,4)
     vykresliPenize
+    
+    % vypis hodnoty
+    disp(['Prumerne hodnoty'])
+    disp(['   delka fronty: ' num2str(round(prumerna_delka_fronty))])
+    disp(['   doba v pekarne: ' num2str(round(prumerna_doba_v_pekarne))])
+    disp(['   doba ve fronte: ' num2str(round(prumerna_doba_ve_fronte))])
+    disp(['Obslouzeno ' num2str(pocet_obslouzenych) ...
+        ' lidi za ' num2str(round(trzba)) ' penez'])
+    disp(['Neobslouzeno ' num2str(pocet_neobslouzenych) ...
+        ' lidi s ' num2str(round(usla_trzba)) ' penezi'])
+end
     
     function vykresliFrontu
         % vykresleni vyvoje fronty
@@ -88,7 +112,6 @@ prumerna_doba_ve_fronte=mean(doba_ve_fronte);
         plot(xlim,[1,1]*prumerna_delka_fronty,'g--');
         
         %yyaxis right
-        %odmitnuti = zakaznici(zakaznici(:,4) == 0,:);
         %stairs(odmitnuti(:,1),cumsum(odmitnuti(:,4)==0),'r') % pocet odmitnutych zakazniku
         %plot([fronta_hist(1,1),fronta_hist(end,1)],[1,1]*prumerna_delka_fronty,'g--');
         title('Vyvoj fronty a poctu zákazníku v pekarne')
@@ -114,10 +137,8 @@ prumerna_doba_ve_fronte=mean(doba_ve_fronte);
     end
 
     function vykresliPenize
-        obslouzen = zakaznici(zakaznici(:,4) == 1,:);
-        stairs(obslouzen(:,1),cumsum(obslouzen(:,5)));
+        stairs(obslouzeni(:,1),cumsum(obslouzeni(:,5)));
         hold on
-        odmitnuti = zakaznici(zakaznici(:,4) == 0,:);
         stairs(odmitnuti(:,1),cumsum(odmitnuti(:,5)),'r');
         hold off
         title('Trzba a usla trzba')
@@ -128,7 +149,7 @@ prumerna_doba_ve_fronte=mean(doba_ve_fronte);
 
     function prichodZakaznika(prichod)
         zakaznici(end+1,1) = prichod; % pridame zakaznika do systemu
-        zakaznici(end,5) = utrata*rand(); %normrnd(); % ochota utratit
+        zakaznici(end,5) = gamrnd(utrata,1); %normrnd(utrata,0.25*utrata); % ochota utratit
         % prichozi zakaznik musi cekat ve fronte nebo se rovnou obslouzi
         if(length(vObsluze) < pocetPrepazek) %existuje volna prepazka
             obsluzDalsihoZakaznika(prichod,size(zakaznici,1));
